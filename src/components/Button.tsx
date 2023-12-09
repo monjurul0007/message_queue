2cf7de54075c6b1d3db6b6c052395abc4e65a0e6
Md Monjurul Hoque Chowdhury <monjurul.hoque.shuvo@gmail.com>
2023-12-09 11:40:22 +0600

    Added navigation button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,12 +2,13 @@ import type { FC, MouseEventHandler, PropsWithChildren } from "react";
 
 interface ButtonProps extends PropsWithChildren {
   onClick?: MouseEventHandler<HTMLButtonElement>;
+  className?: string;
 }
 
-const Button: FC<ButtonProps> = ({ children, onClick }) => {
+const Button: FC<ButtonProps> = ({ children, className, onClick }) => {
   return (
     <button
-      className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+      className={`rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20 ${className}`}
       onClick={onClick}
     >
       {children}
